82b12c2e9ba919fcc56cdea63c6f14acda2928b9
J Marcos <joaomarcos2004.jonh@gmail.com>
2023-04-21 13:28:33 -0300

    upload os projetos mais recentes + novo hub

---
+++ script.js
@@ -0,0 +1,28 @@
+// const fs = require('fs/promises')
+// const path = require('path')
+
+// fs.readdir(path.join('/','Users','cicer','OneDrive','Documentos','vs code','curso 50 projetos','50-projects-in-50-days')).then(files => {
+// //    const projects = files.filter(file => file.includes('project') && file.length == 9)
+// //   console.log(projects)
+
+//   })
+
+// }) 
+
+
+for ( var c = 1 ; c <= 20 ; c++) {
+
+        const div = document.getElementsByClassName('projectslink')[0]
+        const pLink = document.createElement('p')
+        let num = ""
+        if (c < 10) {
+            num = `0${c}`
+        } else { num = c}
+
+        pLink.innerHTML = `<h3><a href="project${num}/index.html" target="_blank" rel="next">project${num}</a></h3>+++`
+    
+        div.appendChild(pLink)
+
+}
+
+   console.log() 
