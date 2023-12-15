433b303c2fcf2456a1db1786ffe928ef75921145
j4marcos <joaomarcos2004.jonh@gmail.com>
2023-12-14 21:30:51 -0300

    update index sistem

--- src/script.js
+++ src/script.js
@@ -1,35 +1,50 @@
- const divLinkCards = document.querySelector('.projectslink')
- const projectslinks = []
- 
- for (let num = 1 ; num < 50 || num == 50 ; num ++) {
-    projectslinks.push(`https://j4marcos.github.io/50-projects-in-50-days/project${String(num).padStart(2,'0')}/index.html`)
- }
+const divLinkCards = document.querySelector('.projectslink')
+const projectslinks = []
+const lista = new Map
+let ordem = 0
 
+for (let num = 1; num < 50 || num == 50; num++) {
+    projectslinks.push(`https://j4marcos.github.io/50ProjectsIn50Days/project${String(num).padStart(2, '0')}/index.html`)
+}
+
+
+projectslinks.forEach((link, num) => {
 const pegarSite = new Promise((sucesso, falha) => {
-    const site = require(link)  
+    const site = fetch(link)
     sucesso(site)
     falha(site)
-})
-
-projectslinks.forEach((link, num) => { 
-    pegarSite
-.then(site => site.text())
-.then(data => {
-    var parser = new DOMParser();
-    var doc = parser.parseFromString(data, 'text/html');
-
-    var pageTitle = doc.title
-    inserirCard(String(num), link, pageTitle)
-})
-.catch(error => console.error(error))
+}) .then(site => site.text())
+        .then(data => {
+            var parser = new DOMParser();
+            var doc = parser.parseFromString(data, 'text/html');
+            var pageTitle = doc.title
+            fila(num, inserirCard(String(num + 1), link, pageTitle))
+        })
+        .catch(error => console.error(error))
 
 })
+
+
+
+function fila(ordem, func) {
+lista.set(ordem,func)
+if (lista.has(ordem)){
+    lista.get(ordem)()
+    ordem++
+    lista.delete(ordem)
+}
+
+
+}
 
 
 function inserirCard(num, link, title) {
-    const card = document.createElement("div.card")
+    const card = document.createElement("div")
+    card.classList.add("card")
     const pLink = document.createElement('h3')
-    pLink.innerHTML = `<a href="${link}" target="_blank" rel="next">${numpadStart(2,"0")} : ${title}</a>`
-    pLink.appendChild(card)
-    card.appendChild(divLinkCards)
+    pLink.innerHTML = `<a href="${link}" target="_blank" rel="next">${num.padStart(2,"0")} : ${title}</a>`
+    divLinkCards.appendChild(card)
+    card.appendChild(pLink)
+    console.log(card)
+
 }
