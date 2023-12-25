a6f7b3797bb441a3736cc2ec6caeba9171e25a19
j4marcos <joaomarcos2004.jonh@gmail.com>
2023-12-25 19:07:24 -0300

    refactor: index list fecth

--- src/script.js
+++ src/script.js
@@ -1,52 +1,29 @@
 const divLinkCards = document.querySelector('.projectslink')
-const projectslinks = []
 const lista = new Map
-let ordem = 1
 
-for (let num = 1; num < 50 || num == 50; num++) {
-    projectslinks.push(`https://j4marcos.github.io/50ProjectsIn50Days/project${String(num).padStart(2, '0')}/index.html`)
-}
-
-
-projectslinks.forEach((link, num) => {
-const pegarSite = new Promise((sucesso, falha) => {
-    const site = fetch(link)
-    sucesso(site)
-    falha(site)
-}) .then(site => site.text())
-        .then(data => {
-            var parser = new DOMParser();
-            var doc = parser.parseFromString(data, 'text/html');
-            var pageTitle = doc.title
-            fila(num, inserirCard(String(num + 1), link, pageTitle))
-        })
-        .catch(error => console.log(error))
-
-})
-
-
-
-
-function fila(n, func) {
-if (lista.has(ordem)){
-    lista.get(ordem)
-    ordem++
-    lista.delete(ordem)
-    fila()
-}
-if (n && func) lista.set(n,func)
-}
-
-
-}
-
-
-function inserirCard(num, link, title) {
+function inserirCard(num, html) {
     const card = document.createElement("div")
     card.classList.add("card")
     const pLink = document.createElement('h3')
-    pLink.innerHTML = `<a href="${link}" target="_blank" rel="next">${num.padStart(2,"0")} : ${title}</a>`
+    const numeral = String(num).padStart(2, "0")
+    pLink.innerHTML = `<a href="project${numeral}" target="_blank" rel="next">${numeral} : ${html.title}</a>`
     divLinkCards.appendChild(card)
     card.appendChild(pLink)
 
 }
+    for (let n = 1; n <= 50; n++) {
+        const getSiteData = fetch(`project${String(n).padStart(2, '0')}`)
+        .then((data) => data.text())
+        .then((data) => {
+            var parser = new DOMParser()
+            var html = parser.parseFromString(data, 'text/html')
+            lista.set(n, html)
+            inserirCard(n,html)
+        })
+    }
+    
+    
+
+
+
+
