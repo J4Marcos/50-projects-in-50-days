eba834ef5ecf00d6eafcc2a3e7b66d1207c890ab
J. Marcos <joaomarcos2004.jonh@gmail.com>
2024-01-02 20:59:31 -0300

    feat: await load cards func

--- src/script.js
+++ src/script.js
@@ -1,5 +1,4 @@
 const divLinkCards = document.querySelector('.projectslink')
-const lista = new Map
 
 function inserirCard(num, html) {
     const card = document.createElement("div")
@@ -11,16 +10,20 @@ function inserirCard(num, html) {
     card.appendChild(pLink)
 
 }
+
+async function getCards() {
     for (let n = 1; n <= 50; n++) {
-        const getSiteData = fetch(`project${String(n).padStart(2, '0')}`)
+        const getSiteData = await fetch(`project${String(n).padStart(2, '0')}`)
         .then((data) => data.text())
         .then((data) => {
             var parser = new DOMParser()
             var html = parser.parseFromString(data, 'text/html')
-            lista.set(n, html)
             inserirCard(n,html)
         })
     }
+}
+
+getCards()
     
     
 
